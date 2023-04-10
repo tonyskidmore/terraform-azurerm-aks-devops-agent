bfb1fc38d4f359d13682a7676806a82b72f27ea5
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-04-10 17:41:01 +0000

    removing aks

--- providers.tf
+++ providers.tf
@@ -28,29 +28,3 @@ terraform {
     }
   }
 }
-
-# https://github.com/hashicorp/terraform-provider-kubernetes/blob/main/_examples/aks/main.tf
-
-provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.default.kube_config[0].host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.default.kube_config[0].host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate)
-  }
-}
-
-provider "kubectl" {
-  host                   = data.azurerm_kubernetes_cluster.default.kube_config[0].host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate)
-  load_config_file       = false
-}
