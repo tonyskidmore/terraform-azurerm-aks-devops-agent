fc9236f1720446deb4b6f42f462f4e07d0e5cb49
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-04-10 12:48:19 +0000

    removing version

--- helm.tf
+++ helm.tf
@@ -2,25 +2,27 @@
 # helm repo update
 # helm install keda kedacore/keda --namespace keda --create-namespace
 
-# https://github.com/matt-FFFFFF/framework/blob/master/modules/aks/main.tf
-
 resource "kubernetes_namespace" "keda" {
   metadata {
     name = "keda"
   }
 }
 
-# data "helm_repository" "kedacore" {
-#   name = "kedacore"
-#   url  = "https://kedacore.github.io/charts"
-# }
+# ╷
+# │ Error: could not download chart: chart "kedacore/keda" version "2.10.0" not found in https://kedacore.github.io/charts repository
+# │
+# │   with module.terraform-azurerm-aks-devops-agent.helm_release.keda,
+# │   on .terraform/modules/terraform-azurerm-aks-devops-agent/helm.tf line 18, in resource "helm_release" "keda":
+# │   18: resource "helm_release" "keda" {
+# │
+# ╵
 
 resource "helm_release" "keda" {
 
   name       = "kedacore"
   repository = "https://kedacore.github.io/charts"
   chart      = "kedacore/keda"
-  version    = "2.10.0"
+  # version    = "2.10.0"
 
   namespace = "keda"
   depends_on = [
--- kubernetes.tf
+++ kubernetes.tf
@@ -71,6 +71,15 @@ resource "kubernetes_job" "azure-pipelines-agent" {
 }
 
 
+# data "kubectl_file_documents" "docs" {
+#     content = file("multi-doc-manifest.yaml")
+# }
+
+# resource "kubectl_manifest" "test" {
+#     for_each  = data.kubectl_file_documents.docs.manifests
+#     yaml_body = each.value
+# }
+
 resource "kubectl_manifest" "scaled_job" {
   yaml_body = templatefile("${path.module}/kubernetes/scaledjob.yml", {
     namespace = var.k8s_ado_agents_namespace
