eee57c9f4dc2f6245c395f9aeccc829cfe7546d5
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-04-11 12:38:49 +0100

    adding pool name variable

--- kubernetes.tf
+++ kubernetes.tf
@@ -97,6 +97,7 @@ resource "kubectl_manifest" "scaled_job" {
   yaml_body = templatefile("${path.module}/kubernetes/scaledjob.yml", {
     namespace = kubernetes_namespace.ado-agents.metadata[0].name
     pool_id   = azuredevops_agent_pool.aks.id
+    pool_name = var.ado_agent_pool_name
     image     = var.k8s_ado_agent_image
   })
   depends_on = [
